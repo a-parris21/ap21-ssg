b7166e7dca04bf674e8578f7d648d8dcf3e2f8a2
Ang3L <>
2022-09-16 16:20:04 -0400

    helperFuncs renamed to helpers, makeDist started

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-import { fstat } from 'fs';
+import fs from 'fs';
 import { path } from 'path';
 import { readline } from 'readline';
 
@@ -13,10 +13,24 @@ export function genHtml(input_str)
 function genHtmlText()
 
 function makeDistFolder() {
-    const dist_path = './dist';
+    const dist_path = '../dist';
 
-    // If the /dist folder exists, remove the folder and all of its contents
-    if (fs
+    // If the /dist directory exists, remove the folder and all of its contents
+    if ( fs.existsSync(dist_path) )
+    {
+        fs.rmSync(dist_path, {
+            recursive: true,
+            force: true
+        });
+    }
+
+    // Create a new /dist directory, if it does not already exist.
+    /* Note: This IF statement may be redundant, since the /dist folder should be deleted 
+       by the above code block before execution reaches this line. */
+    if ( !fs.existsSync(dist_path) )
+    {
+        fs.mkdirSync(dist_path);
+    }
 
     return dist_path;
 }
