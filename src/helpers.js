b7e50d059acb980f8e7b9435e7ebca2ce3957ed1
Ang3L <>
2022-09-16 15:58:59 -0400

    installed fs module

---
+++ src/helpers.js
@@ -0,0 +1,22 @@
+import { fstat } from 'fs';
+import { path } from 'path';
+import { readline } from 'readline';
+
+export function genHtml(input_str)
+{
+    // Filepath for the output directory.
+    const output_dir = makeDistFolder();
+
+
+}
+
+function genHtmlText()
+
+function makeDistFolder() {
+    const dist_path = './dist';
+
+    // If the /dist folder exists, remove the folder and all of its contents
+    if (fs
+
+    return dist_path;
+}
