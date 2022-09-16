f87693008aeb639d000357c09ec54c0d4b77b945
Ang3L <>
2022-09-16 15:44:38 -0400

    updated pckg, added main & generateHtml

---
+++ src/main.js
@@ -0,0 +1,24 @@
+//import {genHtml} from './generateHtml.js';
+import {program} from 'commander';
+
+program
+.option('--version, -v', 'displays the version number')
+.option('--help, -h', 'displays help message')
+.option('--input, -i', 'get input from a specified file or folder');
+
+program.parse(process.argv);
+
+if (program.opts().version)
+{
+    console.log("version: 0.1");
+}
+
+if (program.opts().help)
+{
+    console.log("version: 0.1");
+}
+
+if (program.opts().input)
+{
+    genHtml();
+}
