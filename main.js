61058ecd6e52fc2426e536aea9655dfa2576d73f
Max N <smth@gmail.com>
2022-10-04 07:02:26 +0700

    config file feature added

--- main.js
+++ main.js
@@ -8,15 +8,32 @@ const program = new Command();
 const { version } = require('./package.json');
 const help_message = generateHelpMessage();
 
+const data = require('./config.json');
+
+const configInput = data.input || '';
+const configOutput = data.output || './dist';
+const configStyle = data.stylesheet;
+const configLang = data.lang || 'en';
+
 program.option('-i --input <item>', 'get input')
 	.option('-v --version', 'displays the program name & version number')
 	.option('-h --help', 'displays help message')
 	.option('-l --lang <item>', 'specify the language of the html documents')
-	.option('-o --output <item>', 'specify the output folder where the html files will be generated');
+	.option('-o --output <item>', 'specify the output folder where the html files will be generated')
+	.option('-c --config <item>', 'specify the config file that will contain all SSG options');
 
 program.parse(process.argv);
 
 const options = program.opts();
+
+if(options.config) 
+{
+    generateWebsite(`${configInput}`, `${configOutput}`, `${configStyle || ''}`);
+    setHtmlLang(`${configLang}`);
+}
+
+if(!options.config) 
+{
 
 if (options.version)
 {
@@ -31,7 +48,7 @@ if (options.help)
 if (options.input)
 {
     if (options.output) {
-        generateWebsite(`${options.input}`, `${options.output}`);
+        generateWebsite(`${options.input} `, `${options.output}`);
     }
     else {
         generateWebsite(`${options.input}`, '');
@@ -42,6 +59,9 @@ if (options.lang)
 {
     setHtmlLang(`${options.lang}`);
 }
+
+}
+
 
 function generateHelpMessage()
 {
@@ -58,8 +78,11 @@ function generateHelpMessage()
     helpMsg += `  -v --version               display the program name & version number\n`;
     helpMsg += `  -h, --help                 display this help message\n`;
     helpMsg += `  -i, --input                get input from a specified file or folder\n`;
+    helpMsg += `  -c, --config               specify the config file that will contain all SSG options\n`;
     helpMsg += `----------------------------------------------------------------------\n\n`;
 
 
 	return helpMsg;
 }
+
+export default configStyle;
